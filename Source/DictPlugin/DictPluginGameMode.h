537c749b251583833cb21b3d54ad78d711a81b0a
tsmolinski <tsmolinski3@gmail.com>
2025-06-01 17:42:18 +0200

    Init commit.

---
+++ Source/DictPlugin/DictPluginGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DictPluginGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADictPluginGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADictPluginGameMode();
+};
+
+
+
